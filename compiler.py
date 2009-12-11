b9229c97ec660688539d665c688f19206e69c558
Zellyn Hunter <Zellyn.Hunter@cmgdigital.com>
2009-12-11 15:38:56 -0500

    First commit: compiles to constant 42

---
+++ compiler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+def compile_program(x):
+    emit_function_header("scheme_entry")
+    print "    movl $%d, %%eax" % x
+    print "    ret"
+
+def emit_function_header(function_name):
+    print ".globl _%s" % function_name
+    print "_%s:" % function_name
+
+compile_program(42)
---
+++ runtime.c
@@ -0,0 +1,6 @@
+/* a simple driver for scheme_entry */
+#include <stdio.h>
+int main(int argc, char** argv) {
+    printf("%d\n", scheme_entry());
+    return 0;
+}
